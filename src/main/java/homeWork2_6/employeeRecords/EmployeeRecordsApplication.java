6f26459cfc85e74e2eab969d68c269b601a7583c
Vladimir Muraito <vl.muraito@mail.ru>
2024-07-02 12:22:56 +1000

    Initial commit.

---
+++ src/main/java/homeWork2_6/employeeRecords/EmployeeRecordsApplication.java
@@ -0,0 +1,13 @@
+package homeWork2_6.employeeRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRecordsApplication.class, args);
+	}
+
+}
