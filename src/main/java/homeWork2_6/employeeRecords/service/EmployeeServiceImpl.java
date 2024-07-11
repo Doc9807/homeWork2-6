dfbe0daabc3a5ad50c0cb14018b357cd25bde82b
Vladimir Muraito <vl.muraito@mail.ru>
2024-07-11 15:55:01 +1000

    Проект перенесён на Map.

--- src/main/java/homeWork2_6/employeeRecords/service/EmployeeServiceImpl.java
+++ src/main/java/homeWork2_6/employeeRecords/service/EmployeeServiceImpl.java
@@ -6,18 +6,16 @@ import homeWork2_6.employeeRecords.exception.EmployeeNotFoundException;
 import homeWork2_6.employeeRecords.exception.EmployeeStorageIsFullException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final List<Employee> employeesCompany;
+    private final Map<String, Employee> employeesCompany;
 
     public static final short MAX_EMPLOYEES = 2;
 
-    public EmployeeServiceImpl(List<Employee> employeesCompany) {
-        this.employeesCompany = new ArrayList<>(MAX_EMPLOYEES);
+    public EmployeeServiceImpl(Map<String, Employee> employeesCompany) {
+        this.employeesCompany = new HashMap<>(MAX_EMPLOYEES);
     }
 
     @Override
@@ -25,19 +23,18 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employeeAdd = new Employee(firstName, lastName);
         if (employeesCompany.size() >= MAX_EMPLOYEES) {
             throw new EmployeeStorageIsFullException();
-        } else if (employeesCompany.contains(employeeAdd)) {
+        } else if (employeesCompany.containsKey(employeeAdd.getFullName())) {
             throw new EmployeeAlreadyAddedException();
         }
-        employeesCompany.add(employeeAdd);
+        employeesCompany.put(employeeAdd.getFullName(), employeeAdd);
         return employeeAdd;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
         Employee employeeRemove = new Employee(firstName, lastName);
-        if (employeesCompany.contains(employeeRemove)) {
-            employeesCompany.remove(employeeRemove);
-            return employeeRemove;
+        if (employeesCompany.containsKey(employeeRemove.getFullName())) {
+            return employeesCompany.remove(employeeRemove.getFullName());
         }
         throw new EmployeeNotFoundException();
     }
@@ -45,14 +42,14 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee find(String firstName, String lastName) {
         Employee employeeFind = new Employee(firstName, lastName);
-        if (employeesCompany.contains(employeeFind)) {
-            return employeeFind;
+        if (employeesCompany.containsKey(employeeFind.getFullName())) {
+            return employeesCompany.get(employeeFind.getFullName());
         }
         throw new EmployeeNotFoundException();
     }
 
     @Override
     public Collection<Employee> allEmployee() {
-        return new ArrayList<>(employeesCompany);
+        return Collections.unmodifiableCollection(employeesCompany.values());
     }
 }
